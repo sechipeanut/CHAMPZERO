eb5db44f13b94f180174d0062b4762297076b386
Franz Valles <vallesfranznicole_bsit@plmun.edu.ph>
2025-12-21 13:03:25 +0800

    Fix 404 error handling

--- server.js
+++ server.js
@@ -216,6 +216,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'home.html')); 
 });
 
+// 404 handler - must be last
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
 
 // Start server
 app.listen(PORT, () => {
